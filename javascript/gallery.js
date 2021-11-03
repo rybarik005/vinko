14ea39cbbb43395d0d668d8e087fdaaf364dad9b
mtu4554 <mtu4554@gmail.com>
2021-11-03 19:04:40 +0100

    galéria lg forma hotova

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -1,12 +1,15 @@
-function doFunction(){
-    console.log("clicked");
+window.addEventListener("DOMContentLoaded", (event) => {
+  addItems();
+});
+var parents = document.getElementById("galleryParent");
+var count = 3;
+var galleryItem = loadFile("../assets/htmlTemplates/gallery_item.html");
+function addItems() {
+    console.log("hey");
+    for(var i = 0; i < count; i++) {
+        parents.insertAdjacentHTML( "afterbegin",galleryItem);
+    }
+    var items = parents.getElementsByClassName("carousel-item");
+    items[0].classList.add("active");
 }
 
-function newContent(){
-    console.log("loaded");
-    document.open();
-    document.write("<h1>Out with the old, in with the new!</h1>");
-    document.close();
-}
-
-newContent();
